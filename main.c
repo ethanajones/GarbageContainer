cf670e35b6d551614bfe7fe5d62ed48a6cd2d549
EAJ <eaj9990@gmail.com>
2023-04-04 18:36:42 -0700

    Task 2 Complete

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+    //Task 2
     Filters filters = parse_args(argc, argv);
+    //Task 1
     init_data_source(filters.containers_path, filters.paths_path);
     print_containers(filters);
     destroy_data_source();
